68fef10d8eca103694371e6ed55688e248ed42ca
deltager <github@software.unf.dk>
2017-07-07 14:14:18 +0200

    Fjernet statusbar

--- app/src/main/java/com/example/deltager/endlessrrunnerwithinsults/MainActivity.java
+++ app/src/main/java/com/example/deltager/endlessrrunnerwithinsults/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends Activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 /*
         image1 = (ImageView) findViewById(R.id.obst1);
