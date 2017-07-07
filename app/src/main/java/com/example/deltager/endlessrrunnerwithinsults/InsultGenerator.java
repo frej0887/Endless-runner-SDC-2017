5c68cc19ea4ba1d5df0adeb64874f7859176b669
deltager <github@software.unf.dk>
2017-07-07 21:05:19 +0200

    Completed InsultGenerator and added insults.

--- app/src/main/java/com/example/deltager/endlessrrunnerwithinsults/InsultGenerator.java
+++ app/src/main/java/com/example/deltager/endlessrrunnerwithinsults/InsultGenerator.java
@@ -26,7 +26,8 @@ public class InsultGenerator {
                 "You're a failed abortion whose birth certificate is an apology from the condom factory.", "You'll never be the man your mother is.",
                 "If I wanted to kill myself I'd climb your ego and jump to your IQ.", "If ignorance is bliss, you must be the happiest person on earth.",
                 "I wasn't made with enough bad words to let you know how I feel about you.", "You are proof that evolution CAN go in reverse.",
-                "So, a thought crossed your mind? Must have been a long and lonely journey."
+                "So, a thought crossed your mind? Must have been a long and lonely journey.", "I'm jealous of all the people that haven't met you!",
+                "I would love to insult you... but that would be beyond the level of your intelligence.",
                 };
 
 
