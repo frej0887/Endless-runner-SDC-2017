da752b767e38f42e9e51c48ea776df5b0f181946
deltager <github@software.unf.dk>
2017-07-09 18:05:37 +0200

    Baggrund er lavet og animerer

--- app/src/main/java/com/example/deltager/endlessrrunnerwithinsults/viewGame.java
+++ app/src/main/java/com/example/deltager/endlessrrunnerwithinsults/viewGame.java
@@ -26,9 +26,10 @@ public class viewGame extends View
     Paint obstColour, playerColour, textColour;
     Timer timing;
     Bitmap background, træ1, player, deathscreen;
+    Bitmap b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18;
     boolean init, onDeathInsult;
     boolean printInsult = false;
-    int deathCnt = 0;
+    int ct;
 
     InsultGenerator insultGenerator = new InsultGenerator(getContext());
 
@@ -51,27 +52,38 @@ public class viewGame extends View
         setup(context);
     }
 
-    public void setup(Context context)
-    {
+    public void setup(Context context) {
         onDeathInsult = false;
         //TODO: Hent alt den grafik I skal bruge ind i feltvariabler
         træ1 = BitmapFactory.decodeResource(this.getResources(), R.mipmap.traeer_stor_web);
-        player = BitmapFactory.decodeResource(this.getResources(), R.mipmap.ballweb);
+        player = BitmapFactory.decodeResource(this.getResources(), R.mipmap.ball);
         deathscreen = BitmapFactory.decodeResource(this.getResources(), R.mipmap.deathscreenweb);
         //TODO: Brug den her som constructor for viewGame
         makeNewGame(context);
 
+        b1 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b1);
+        b2 = BitmapFactory.decodeResource(this.getResources(),R.drawable.b2);
+        b3 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b3);
+        b4 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b4);
+        b5 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b5);
+        b6 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b6);
+        b7 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b7);
+        b8 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b8);
+        b9 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b9);
+        b10 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b10);
+        b11 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b11);
+        b12 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b12);
+        b13 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b13);
+        b14 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b14);
+        b15 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b15);
+        b16 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b16);
+        b17 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b17);
+        b18 = BitmapFactory.decodeResource(this.getResources(), R.drawable.b18);
 
     }
 
-    public int getDeathCnt() {
-        return deathCnt;
-    }
-    public void makeNewGame(Context gameContext)
-    {
-        deathCnt++;
-        if(!(timing == null)&&timing.isAlive())
-        {
+    public void makeNewGame(Context gameContext)    {
+        if(!(timing == null)&&timing.isAlive())        {
             timing.interrupt();
         }
         game = new Game(gameContext);
@@ -83,7 +95,6 @@ public class viewGame extends View
         postInvalidate();
         timing = new Timer();
         timing.start();
-
     }
 
     public InsultGenerator getInsultGenerator() {
@@ -99,16 +110,14 @@ public class viewGame extends View
     }
 
     @Override
-    protected void onDraw(Canvas canvas)
-    {
+    protected void onDraw(Canvas canvas) {
         //TODO: Tegn alt I jeres spil med canvas.drawBitMap(), canvas.drawRect() etc.
         //TODO: I kan få fat i jeres obstacles og spiller med game.getPlayer() og game.getObstacles()
 
         //Width and height
         height = canvas.getHeight();
         width = canvas.getWidth();
-        if (init)
-        {
+        if (init)    {
             init = false;
 
             //Player pos
@@ -118,6 +127,24 @@ public class viewGame extends View
             //Image scaling
             træ1 = Bitmap.createScaledBitmap(træ1, (int) (0.3 * width) + 1, (int) (0.3 * width) + 1, true);
 
+            b1 = Bitmap.createScaledBitmap(b1, width, height , true);
+            b2 = Bitmap.createScaledBitmap(b2, width, height, true);
+            b3 = Bitmap.createScaledBitmap(b3, width, height, true);
+            b4 = Bitmap.createScaledBitmap(b4, width, height, true);
+            b5 = Bitmap.createScaledBitmap(b5, width, height, true);
+            b6 = Bitmap.createScaledBitmap(b6, width, height, true);
+            b7 = Bitmap.createScaledBitmap(b7, width, height, true);
+            b8 = Bitmap.createScaledBitmap(b8, width, height, true);
+            b9 = Bitmap.createScaledBitmap(b9, width, height, true);
+            b10 = Bitmap.createScaledBitmap(b10, width, height, true);
+            b11 = Bitmap.createScaledBitmap(b11, width, height, true);
+            b12 = Bitmap.createScaledBitmap(b12, width, height, true);
+            b13 = Bitmap.createScaledBitmap(b13, width, height, true);
+            b14 = Bitmap.createScaledBitmap(b14, width, height, true);
+            b15 = Bitmap.createScaledBitmap(b15, width, height, true);
+            b16 = Bitmap.createScaledBitmap(b16, width, height, true);
+            b17 = Bitmap.createScaledBitmap(b17, width, height, true);
+            b18 = Bitmap.createScaledBitmap(b18, width, height, true);
         }
         //Background colour
 
@@ -126,9 +153,74 @@ public class viewGame extends View
         backColor.setColor(Color.GREEN);
         backColor2.setColor(Color.BLACK);
 
+
+
         //Background
         canvas.drawRect(0, 0, width, height, backColor);
 
+
+
+        switch(ct)    {
+            case 0:
+                canvas.drawBitmap(b1, 0, 0, null);
+                break;
+            case 1:
+                canvas.drawBitmap(b2, 0, 0, null);
+                break;
+            case 2:
+                canvas.drawBitmap(b3, 0, 0, null);
+                break;
+            case 3:
+                canvas.drawBitmap(b4, 0, 0, null);
+                break;
+            case 4:
+                canvas.drawBitmap(b5, 0, 0, null);
+                break;
+            case 5:
+                canvas.drawBitmap(b6, 0, 0, null);
+                break;
+            case 6:
+                canvas.drawBitmap(b7, 0, 0, null);
+                break;
+            case 7:
+                canvas.drawBitmap(b8, 0, 0, null);
+                break;
+            case 8:
+                canvas.drawBitmap(b9, 0, 0, null);
+                break;
+            case 9:
+                canvas.drawBitmap(b10, 0, 0, null);
+                break;
+            case 10:
+                canvas.drawBitmap(b11, 0, 0, null);
+                break;
+            case 11:
+                canvas.drawBitmap(b12, 0, 0, null);
+                break;
+            case 12:
+                canvas.drawBitmap(b13, 0, 0, null);
+                break;
+            case 13:
+                canvas.drawBitmap(b14, 0, 0, null);
+                break;
+            case 14:
+                canvas.drawBitmap(b15, 0, 0, null);
+                break;
+            case 15:
+                canvas.drawBitmap(b16, 0, 0, null);
+                break;
+            case 16:
+                canvas.drawBitmap(b17, 0, 0, null);
+                break;
+            case 17:
+                canvas.drawBitmap(b18, 0, 0, null);
+                ct = 0;
+                break;
+        }
+
+        //Colour of the obstacle
+        obstColour = new Paint();
+        obstColour.setColor(Color.BLUE);
 
         //Arraylist of the current obstacles
         ArrayList<obstacles> obstacles = game.getObstacles();
@@ -136,7 +228,6 @@ public class viewGame extends View
         //Draw obstacle for ones which are currently in use
         for (obstacles o : obstacles)
         {
-            //TODO Crash her pga multiple access
             float p = (float) (0.05 * width + o.getPath() * 0.3 * width);
             o.setxPos(p);
             float temp = (float) (0.3 * width);
@@ -158,13 +249,18 @@ public class viewGame extends View
 
         checkObstaclesPassed(game.getObstPassed());
 
-        if(!game.getIsAlive()){
+        if(onDeathInsult){
+            //Make insult when you die
+            insultGenerator.insult(0);
+            onDeathInsult = false;
+        if(!game.getIsAlive()) {
             //Lav et insult når playeren dør
             //insultGenerator.insult(0);
             game.setObstPassed(0);
             canvas.drawRect(0, 0, width, height, backColor2);
-            deathscreen = Bitmap.createScaledBitmap(deathscreen, width, (int) (0.85366*width), true);
-            canvas.drawBitmap(deathscreen, 0, height/2 - deathscreen.getHeight()/2, null);
+            deathscreen = Bitmap.createScaledBitmap(deathscreen, width, (int) (0.85366 * width), true);
+            canvas.drawBitmap(deathscreen, 0, height / 2 - deathscreen.getHeight() / 2, null);
+        }
         }
     }
 
@@ -177,35 +273,34 @@ public class viewGame extends View
     {
 
         @Override
-        public void run()
-        {
-            try
-            {
-            while (game.getIsAlive())
-            {
+        public void run()  {
+            try   {
+                int j = 0;
+                while (game.getIsAlive())    {
                     Thread.sleep(1000 / 60);
-                    //60fps
-
-                insultTimer++;
-                if (insultTimer >= 300){
+                    //60fp
+                    j++;
+                    insultTimer++;
+                if (insultTimer >= 300)    {
                     printInsult = true;
                 }
-                for (obstacles o : game.getObstacles())
-                {
+                if (j == 10)    {
+                    ct++;
+                    j = 0;
+                }
+                for (obstacles o : game.getObstacles())   {
                     o.setyPos((float)(o.getyPos() + width*.02));
                     game.coll(o.getxPos(), o.getyPos(), width);
                 }
                 game.checkObstacles(height);
-                //TODO: Få obstacles til at spawne på korrekte tidspunkter
-
 
                 //TODO: Kald postInvalidate() når grafik skal opdateres
                 postInvalidate();
-            }
+                }
             onDeathInsult = true;
             postInvalidate();
-                } catch (InterruptedException e)
-            {
+            }
+                catch (InterruptedException e)  {
                 //Do nothing here
             }
         }
